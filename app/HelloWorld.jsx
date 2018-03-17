4d9b0080cce108a57e7ef2ee2374991cedb48631
Raul Tomescu <tomescu.raul@gmail.com>
2018-03-17 22:13:00 +0100

    Server will not start because of unexpected token

--- app/HelloWorld.jsx
+++ app/HelloWorld.jsx
@@ -1,8 +1,15 @@
 // Dead simple component for the hello world (hi mom!)
 
 import React from 'react';
+import {Separator} from '@everreal/er-common-react-components/lib/components/common/Separator/Separator';
+
 import './HelloWorld.scss';
 
 export default function HelloWorld() {
-  return <h1 className="hello-world">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="hello-world">Hello world!</h1>
+      <Separator />
+    </div>
+  );
 }
